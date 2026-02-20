eda6c1fbf8b8e62a30fc2eb13bb5aad39f16c598
Tobiasdfsfsd89 <92136638+Tobiasdfsfsd89@users.noreply.github.com>
2026-02-20 21:20:18 +0000

    Almost end of Chapter 1

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -4,20 +4,29 @@
 
 #include <iostream>
 
-int main(){
-    float num_1, num_2, result;
+// int main(){
+//     float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
-    std::cin >> num_2;
+//     std::cout << "Enter number 1: " << std::flush;
+//     std::cin >> num_1;
+//     std::cout << "Enter number 2: " << std::flush;
+//     std::cin >> num_2;
 
-    result = num_1 + num_2;
+//     result = num_1 + num_2;
 
-    std::cout << "The result of the addition is " << result << std::endl;
+//     std::cout << "The result of the addition is " << result << std::endl;
 
-    std::cout << std::endl << std::endl;
-    return 0;
+//     std::cout << std::endl << std::endl;
+//     return 0;
+// }
+
+// Function declaration
+int add(int a, int b){
+    return a + b; // Return the sum of a and b
 }
 
-
+int main(){
+    int result = add(15,20); // Function call with argument 5 and 3
+    std::cout << "THe sum is: " << result << std::endl;
+    return 0;
+}
--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -3,12 +3,33 @@
 // Hello World, by Eduardo Corpeño 
 
 #include <iostream>
+// #include <cstdio> // Adds C language input and output functionality
 
-int main(){
-    std::cout << "Hello World!!" << std::endl;
+// int main(){
+//     std::cout << "Hello World!!" << std::endl;
 
-    std::cout << std::endl << std::endl;
+//     std::cout << std::endl << std::endl;
+//     return 0;
+
+// }
+
+
+/* This program prints
+    the message "Hi There!"
+    in the terminal output. */
+
+// int main(){ // This is the Main function
+//     std::cout << "Hi There!" << std::endl;
+//     return 0;
+// }
+
+// int main(){
+//     printf("Hey, there!\n");
+//     return 0;
+// }
+
+//Command line terminal code
+int main(int argc, char* argv[]){
+    std::cout << "Hi There!!" << std::endl;
     return 0;
-
-
 }
