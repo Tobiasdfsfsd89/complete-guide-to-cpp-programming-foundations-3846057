43920e7d726d0694633a89e539fa4c5a1c28efa0
Tobiasdfsfsd89 <92136638+Tobiasdfsfsd89@users.noreply.github.com>
2026-02-20 20:33:38 +0000

    added a commit

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -11,11 +11,13 @@ int main(){
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
-    
+
     result = num_1 + num_2;
-    
+
     std::cout << "The result of the addition is " << result << std::endl;
-    
+
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+
