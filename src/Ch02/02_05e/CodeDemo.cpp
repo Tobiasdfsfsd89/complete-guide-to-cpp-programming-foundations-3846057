d49e66647c5dc4855d24f6a5ccc2fa4c2fdb4b8f
Tobiasdfsfsd89 <92136638+Tobiasdfsfsd89@users.noreply.github.com>
2026-03-12 22:35:43 +0000

    Changes before the restart

--- src/Ch02/02_05e/CodeDemo.cpp
+++ src/Ch02/02_05e/CodeDemo.cpp
@@ -34,12 +34,61 @@
 //     return 0;
 // }
 
-constexpr int calculateExperience(int level){
-    return level * level * 100;
-}
+// constexpr int calculateExperience(int level){
+//     return level * level * 100;
+// }
+
+// int main(){
+//     constexpr int experience = calculateExperience(5);
+//     std::cout << "Experience for level 5: " << experience << std::endl;
+//     return 0;
+// }
+
+// Combined Qualifiers
+// constexpr int calculateMaxSpeed(int baseSpeed, int boost){
+//     return baseSpeed + boost * 5;
+// }
+
+// constexpr const int maxSpeed = calculateMaxSpeed(60,8);
+
+// void generateUniqueID(){
+//     static int id = 0;
+//     ++id;
+//     std::cout << "Genreated ID: " << id << std::endl;
+// }
+
+// int main(){
+//     generateUniqueID(); //Output: Generated ID: 1
+//     generateUniqueID(); //Output: Generated ID: 2
+//     generateUniqueID(); //Output: Generated ID: 3
+//     return 0;
+// }
+
+class Enemy{
+public:
+    static int enemyCount; // Static class member
+
+    // This is the constructor, called every time a new Enemy object is instantiated.
+    Enemy(){
+        ++enemyCount; //Increase enemy count
+    }
+
+    static void showEnemyCount(){
+        std::cout << "Total Enemies: " << enemyCount << std::endl;
+    }
+
+
+
+};
+
+// Initialize static member even when no instances exist yet
+int Enemy::enemyCount = 0;
 
 int main(){
-    constexpr int experience = calculateExperience(5);
-    std::cout << "Experience for level 5: " << experience << std::endl;
+    Enemy goblin;
+    Enemy orc;
+    Enemy troll;
+
+    Enemy::showEnemyCount(); //Output: Total Enemies: 3
     return 0;
 }
--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,15 +5,20 @@
 #include <iostream>
 #include <cstdint>
 
-enum asset_type {texture, sound, animation, script};
+// texture = 0, sound = 1, animation = 2, script = 3
+enum class asset_type {texture, sound, animation, script}; //C-style enumeration style called asset type
+enum class menu_section {background, music, sound, controls, texture};
+
 
 int main(){
-    int asset_value;
+    int sound = 8;
+    asset_type asset_value;
 
-    asset_value = sound;
+    asset_value = asset_type::sound;
 
-    std::cout << "asset_value = " << asset_value << std::endl;
+    std::cout << "asset_value = " << (int) asset_value << std::endl; // the asset_type doesn't work with cout so asset_value has to be casted
 
     std::cout << std::endl << std::endl;
+
     return 0;
 }
